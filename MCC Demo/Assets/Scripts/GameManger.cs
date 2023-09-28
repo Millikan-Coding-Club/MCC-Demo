620e2cba0d4d47c93850f7a51e2b369d87cf57bd
Steve <goldenskgaming@gmail.com>
2023-09-27 18:35:28 -0700

    Version 0.2
    - Added Count to Game Manager
    - Added Player

--- MCC Demo/Assets/Scripts/GameManger.cs
+++ MCC Demo/Assets/Scripts/GameManger.cs
@@ -7,7 +7,7 @@ public class GameManger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int count = 0;
     }
 
     // Update is called once per frame
